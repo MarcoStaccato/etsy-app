7b8baee51899bda5595d65d0c594e3c5784a0cb7
Marco Staccato <marcostaccato@gmail.com>
2015-01-12 22:39:13 -0600

    first rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EtsyApp::Application.config.secret_key_base = '6b02ffb1b0d43e7564e22c976e4853f0ac7be2baabaf3881943773ada683efed660c30a682eb327daaa65953e09ffbef82496dae4a1de5c60e5655787e71f7b5'
